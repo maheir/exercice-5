347e762329bd6ae9e3d2b4fbb0f37e3d68e732f2
maheir <maheirne@gmail.com>
2018-12-17 16:39:29 +0100

    VARIABLE EXERCICE 5

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercice_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a, b;
+            DateTime datetime = DateTime.UtcNow.Date;
+            Console.WriteLine("name");
+            a= Console.ReadLine();
+            Console.WriteLine("surname");
+            b= Console.ReadLine();
+            Console.WriteLine("Bonjour " + a + " " + b +" ,nous sommes le "+ datetime.ToString("dd/MM/yyyy ")+ ",comment allez-vous?");
+
+        }
+    }
+}
